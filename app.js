719799c4a3df3c84e31b67d724af9442e91f916e
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-10-05 11:57:46 +0300

    Middleware

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.use((req, res, next) => {
+    console.log("middleware 1 çalıştırıldı");
+    next()
 });
 
-app.get('/api/products', (req, res) => {
-    res.send('Products List');
-});
+app.use((req, res, next) => {
+    console.log("middleware 2 çalıştırıldı")
+    res.send('<h1>Hello from express</h1>');
+})
 
 app.listen(3000, () => {
     console.log('Listenin on port 3000.');
