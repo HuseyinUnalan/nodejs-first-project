f8b81d609d4f1f0fde74f37903f7626db08f02bf
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-10-05 12:12:38 +0300

    Body Parser

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-    console.log('Loglama yapıldı...');
-    next();
+app.get('/add-product', (req, res, next) => {
+    res.send(
+        `
+      <html>
+        <head><title>Add a New Product</title></head>
+          <body>
+            <form action="/product" method="POST">
+              <input type="text" name="productName">
+              <input type="submit" value="Save Product">
+            </form>
+          </body>
+      </html>
+      `
+    );
 })
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<h1>Adding product page</h1>');
-})
-
-app.use('/product-list', (req, res, next) => {
-    res.send('<h1>Product listing page</h1>');
-})
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello from express</h1>');
