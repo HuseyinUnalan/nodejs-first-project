313338ec9e0ca665177a5aaf1334e30d7c99af7e
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-10-05 12:21:18 +0300

    Express Router

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin')
+const userRoutes = require('./routes/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/add-product', (req, res, next) => {
-    res.send(
-        `
-      <html>
-        <head><title>Add a New Product</title></head>
-          <body>
-            <form action="/product" method="POST">
-              <input type="text" name="productName">
-              <input type="submit" value="Save Product">
-            </form>
-          </body>
-      </html>
-      `
-    );
-})
+// Routes
+app.use('/admin', adminRoutes);
+app.use(userRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from express</h1>');
-})
 
 app.listen(3000, () => {
     console.log('Listenin on port 3000.');
---
+++ routes/admin.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/add-product', (req, res, next) => {
+    res.send(
+        `
+    <html>
+      <head><title>Add a New Product</title></head>
+        <body>
+          <form action="/admin/add-product" method="POST">
+            <input type="text" name="productName">
+            <input type="submit" value="Save Product">
+          </form>
+        </body>
+    </html>
+    `
+    );
+})
+
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
