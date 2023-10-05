6bd2141ff60ff59a209a81cca4b7845d6936f50c
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-10-05 12:03:20 +0300

    Routing

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("middleware 1 çalıştırıldı");
-    next()
-});
 
-app.use((req, res, next) => {
-    console.log("middleware 2 çalıştırıldı")
+app.use('/', (req, res, next) => {
+    console.log('Loglama yapıldı...');
+    next();
+})
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>Adding product page</h1>');
+})
+
+app.use('/product-list', (req, res, next) => {
+    res.send('<h1>Product listing page</h1>');
+})
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello from express</h1>');
 })
 
