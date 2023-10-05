69b14e283f95fc5967141f29ddad0bb768761305
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-10-05 12:37:03 +0300

    HTML Page Submission

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin')
 const userRoutes = require('./routes/user');
@@ -13,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(userRoutes);
 
 app.use((req, res) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
