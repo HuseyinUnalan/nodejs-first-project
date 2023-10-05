3fcbb780153f8cf85202693db4a31475616d12fd
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-10-05 22:02:07 +0300

    Converting Html Pages to Pug File

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,9 @@ const path = require('path');
 
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
-})
+    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.render('add-product');
+});
 
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
