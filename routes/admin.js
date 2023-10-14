8a13eef22dfee8ac1d965010cd8372d12b025cfa
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-10-14 20:08:25 +0300

    Add Product Page

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const path = require('path');
+// const path = require('path');
+
+const products = [
+    { name: 'Samsung S8', price: 3000, image: 'e.jpg', description: 'İyi' },
+    { name: 'Samsung S7', price: 2000, image: 'b.jpg', description: 'İdare Eder' },
+    { name: 'Samsung S9', price: 4000, image: 'c.jpg', description: 'Güzel' },
+    { name: 'Iphone X', price: 45000, image: 'd.jpg', description: 'Çok İyi' },
+]
 
 
 router.get('/add-product', (req, res, next) => {
@@ -11,7 +18,14 @@ router.get('/add-product', (req, res, next) => {
 
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
+    products.push({
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        description: req.body.description
+    });
     res.redirect('/');
 });
 
-module.exports = router;
+module.exports.routes = router;
+module.exports.products = products;
