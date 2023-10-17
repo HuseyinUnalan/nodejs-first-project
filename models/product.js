be0ed50207b9296208c31c3ac41b236fb8beca63
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-10-17 20:35:12 +0300

    Random Product ID

--- models/product.js
+++ models/product.js
@@ -1,10 +1,14 @@
 const products = [
-    { name: 'Samsung S6', price: 2000, imageUrl: 'e.jpg', description: 'İyi Ürün' }
+    { id: "15151", name: 'Samsung S6', price: 2000, imageUrl: 'e.jpg', description: 'İyi Ürün' },
+    { id: "14141", name: 'Samsung S8', price: 1500, imageUrl: 'd.jpg', description: 'Güzel Ürün' }
+
+
 ];
 
 module.exports = class Product {
 
     constructor(name, price, imageUrl, description) {
+        this.id = Math.floor(Math.random() * 99999) + 1;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
