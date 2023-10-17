e1fbab0f70bc8613d86063d3a3548c099b3ac84c
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-10-17 14:19:02 +0300

    Add Admin Controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,23 +1,48 @@
-// const Product = require('../models/product')
+const Product = require('../models/product')
 
-// module.exports.getAddProducts = (req, res, next) => {
-//     // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
-//     res.render('add-product', {
-//         title: 'Add a New Product',
-//         path: '/admin/add-product'
 
-//     });
-// }
+module.exports.getProducts = (req, res, next) => {
+    const products = Product.getAll();
+    res.render('admin/products', {
+        title: 'Admin Products',
+        products: products,
+        path: 'admin/products'
 
-// exports.postAddProducts = (req, res, next) => {
-//     const product = new Product(
-//         req.body.name,
-//         req.body.price,
-//         req.body.imageUrl,
-//         req.body.description
-//     );
-//     product.saveProduct();
+    });
+}
 
-//     console.log(req.body);
-//     res.redirect('/');
-// }
+module.exports.getAddProducts = (req, res, next) => {
+    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.render('admin/add-product', {
+        title: 'Add a New Product',
+        path: '/admin/add-product'
+
+    });
+}
+
+exports.postAddProducts = (req, res, next) => {
+    const product = new Product(
+        req.body.name,
+        req.body.price,
+        req.body.imageUrl,
+        req.body.description
+    );
+    product.saveProduct();
+
+    console.log(req.body);
+    res.redirect('/');
+}
+
+module.exports.getEditProducts = (req, res, next) => {
+    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.render('admin/edit-product', {
+        title: 'Edit Product',
+        path: '/admin/edit-product'
+
+    });
+}
+
+exports.postEditProducts = (req, res, next) => {
+
+    res.redirect('/');
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const productsController = require('../controllers/shop')
+const adminController = require('../controllers/admin')
 
 
 // const path = require('path');
 
-// router.get('/add-product', productsController.getAddProducts);
+router.get('/add-product', adminController.getAddProducts);
 
-// router.post('/add-product', productsController.postAddProducts)
+router.post('/add-product', adminController.postAddProducts);
+
+router.get('/edit-product', adminController.getEditProducts);
+
+router.post('/edit-product', adminController.postEditProducts)
+
+router.get('/products', adminController.getProducts);
 
 module.exports = router;
