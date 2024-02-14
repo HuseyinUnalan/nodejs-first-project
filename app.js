c77eccf0007ac54095eb8e9185717c87e57ee0d8
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2024-02-14 11:56:35 +0300

    Sequelize Database Table Creation

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/shop');
 const errorController = require('./controllers/errors');
 
-const sequelize = require('./utility/database');
+const sequelize = require('./utility/database')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,20 +22,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(userRoutes);
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
-
-
 
 app.use(errorController.get404Page);
 
+sequelize.sync()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 app.listen(3000, () => {
     console.log('Listenin on port 3000.');
